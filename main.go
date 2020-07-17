904dcb28808dc14a17d58ef4a8200ac523bd6703
suryareddy3301 <suryareddy3301@gmail.com>
2020-07-17 11:27:05 +0530

    logging capabilities

--- main.go
+++ main.go
@@ -2,26 +2,34 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"text/template"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+var log *Logger
+
 func main() {
+	log, f := NewLogger()
+
 	router := httprouter.New()
+	log.LogInfo("Creating new server")
 	server, err := NewServer()
 	if err != nil {
+		log.LogWarningf("Could not create server from config, creating default. Error %v", err)
 		server, err = CreateConfig()
 		if err != nil {
+			log.LogErrorf("Could not create default server instance. Error %v", err)
 			Error(router, err)
 		}
 	}
 	server.Router = router
 	router.ServeFiles("/static/*filepath", http.Dir("/etc/portfolio/assets"))
 	router.GET("/", server.Index)
+	log.LogInfo("Server started")
 	log.Fatal(http.ListenAndServe(":8080", router))
+	//	f.Close()
 }
 
 func (srv *Server) Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
